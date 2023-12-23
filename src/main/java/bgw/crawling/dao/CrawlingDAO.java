238e0a79c17aba2c0de4864f0ce9b11e30faff90
back <bgwbgw1001@gmial.com>
2023-12-23 23:58:56 +0900

    sql 로그 코드 개선

--- src/main/java/bgw/crawling/dao/CrawlingDAO.java
+++ src/main/java/bgw/crawling/dao/CrawlingDAO.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -23,8 +24,10 @@ public class CrawlingDAO {
     private CrawlingDAO(){}
 
     public void insert(List<CrawlingVO> dataList){
+        ArrayList<Object> paramList = new ArrayList<>();
         StringBuilder sql = new StringBuilder();
         StringBuilder sqlQuery = new StringBuilder();
+
         sql.append("INSERT INTO LIST").append("\n")
                 .append("(PLAT_FORM,USER_ID,NAME,TITLE,VIEWS,UPDATE_DT)").append("\n")
                 .append("SELECT '' AS PLAT_FORM").append("\n")
@@ -35,11 +38,9 @@ public class CrawlingDAO {
                 .append("       ,NOW() AS UPDATE_DT").append("\n")
                 .append("FROM DUAL").append("\n")
                 .append("WHERE '' != ''").append("\n");
-        sqlQuery.append(sql);
+
         for (CrawlingVO crawlingVO : dataList) {
-            String platForm = "non";
-            platForm = crawlingVO instanceof AfricaVO ? "africa" : platForm;
-            platForm = crawlingVO instanceof TwitchVO ? "twitch" : platForm;
+
             sql.append("UNION ALL").append("\n")
                     .append("SELECT ? AS PLAT_FORM").append("\n")
                     .append("       ,? AS USER_ID").append("\n")
@@ -48,18 +49,11 @@ public class CrawlingDAO {
                     .append("       ,? AS VIEWS").append("\n")
                     .append("       ,NOW() AS UPDATE_DT").append("\n")
                     .append("FROM DUAL").append("\n");
-            sqlQuery.append("UNION ALL").append("\n")
-                    .append("SELECT '").append(platForm).append("' AS PLAT_FORM ").append("\n")
-                    .append("       ,'").append(crawlingVO.getUserId()).append("' AS USER_ID ").append("\n")
-                    .append("       ,'").append(crawlingVO.getName()).append("' AS NAME").append("\n")
-                    .append("       ,'").append(crawlingVO.getTitle()).append("' AS TITLE").append("\n")
-                    .append("       ,").append(crawlingVO.getViews()).append(" AS VIEWS").append("\n")
-                    .append("       ,NOW() AS UPDATE_DT").append("\n")
-                    .append("FROM DUAL").append("\n");
         }
-        sql.append("ON DUPLICATE KEY UPDATE NAME = VALUES(NAME), TITLE = VALUES(TITLE), VIEWS = VALUES(VIEWS)");
-        sqlQuery.append("ON DUPLICATE KEY UPDATE NAME = VALUES(NAME), TITLE = VALUES(TITLE), VIEWS = VALUES(VIEWS)");
 
+        sqlQuery.append(" query execute :  ").append("\n").append(sql.toString().replaceAll("\\?","{}"));
+
+        sql.append("ON DUPLICATE KEY UPDATE NAME = VALUES(NAME), TITLE = VALUES(TITLE), VIEWS = VALUES(VIEWS)");
 
         try (PreparedStatement statement = connection.prepareStatement(sql.toString())){
 
@@ -73,16 +67,22 @@ public class CrawlingDAO {
                 statement.setString(index++,crawlingVO.getName());
                 statement.setString(index++,crawlingVO.getTitle());
                 statement.setInt(index++,crawlingVO.getViews());
+                paramList.add(platForm);
+                paramList.add(crawlingVO.getUserId());
+                paramList.add(crawlingVO.getName());
+                paramList.add(crawlingVO.getTitle());
+                paramList.add(crawlingVO.getViews());
             }
 
-            //log.info(sqlQuery.toString());
+            log.debug(sqlQuery.toString(),paramList.toArray());
             statement.executeUpdate();
+
         } catch (SQLException e) {
-            log.info(sqlQuery.toString());
-            log.error("createStatement Fail", e);
+
+            log.info( sqlQuery.toString(),paramList.toArray());
+            log.error("statement Fail", e);
         }
     }
-
 
 
 
